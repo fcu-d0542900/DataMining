29dd74d7d1b47feb784a582aa0da7b2760fa3fb7
yuru <d0542900@mail.fcu.edu.tw>
2019-11-26 10:56:44 +0800

    build db

---
+++ mlb.R
@@ -0,0 +1,15 @@
+library(RSQLite)
+
+url <- "D:/YURU/Documents/YURU/CS/R/DataMining/"
+setwd(url)
+
+mlb_record <- read.csv(file="MLB_Game_Record.csv", header=TRUE, sep=",")
+mlb_player <- read.csv(file="MLB_Player_Id.csv", header=TRUE, sep=",")
+
+drv <- dbDriver("SQLite")
+db <- dbConnect(drv, "data.db")
+dbWriteTable(db, "mlb_record", mlb_record)
+dbReadTable(db, "mlb_record")
+dbWriteTable(db, "mlb_player", mlb_player)
+dbReadTable(db, "mlb_player")
+dbListTables(db)
